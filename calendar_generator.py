1ca7236bf174866a60f0eabbc0dea36fe5aa3478
asadbek-axtamov <asadbekakhtamov20@gmail.com>
2026-01-18 21:53:12 +0500

    edited

--- calendar_generator.py
+++ calendar_generator.py
@@ -3,27 +3,15 @@ class CalendarGenerator:
         self.year = year
         self.month = month
 
-    def is_leap_year(y1):
-        if y1.year % 400 != 0 and y1.year % 100 == 0:
+    def is_leap_year(self):
+        if self.year % 400 != 0 and self.year % 100 == 0:
             return False
-        elif y1.year % 4 != 0:
+        elif self.year % 4 != 0:
             return False
         else:
             return True
     
-    def get_days_in_month(self):
-        return 
-    
-    def get_start_day_of_month(self):
-        return 
-    
-    def build_grid_string(self):
-        return
-
-    def generate_calendar(self):
-        return 
 
 
 check_leap_year = CalendarGenerator(2024, 1)
 print(check_leap_year.is_leap_year())
-
