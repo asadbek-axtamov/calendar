7c13d2c61698fa19661912189028d27e11cbefd6
asadbek-axtamov <asadbekakhtamov20@gmail.com>
2026-01-20 09:49:58 +0500

    get_start_day_of_month is done

--- calendar_generator.py
+++ calendar_generator.py
@@ -31,7 +31,15 @@ class CalendarGenerator:
         return months[self.month]
     
     def get_start_day_of_month(self):
-        return 
+        if self.month<3:
+            self.month += 12
+            self.year -= 1
+        q = 1
+        m = self.month
+        K = self.year%100
+        J = (self.year)//100
+        h = ((q + 13*(m+1)//5+K + K//4 + J//4 - 2*J)%7)
+        return (h+6)%7
     
     def build_grid_string(self):
         return
@@ -40,8 +48,11 @@ class CalendarGenerator:
         return 
 
 
-check_leap_year = CalendarGenerator(2024, 1)
+check_leap_year = CalendarGenerator(2024,1)
 #print(check_leap_year.is_leap_year())
 
 days = CalendarGenerator(2024,5)
-print(days.get_days_in_month())
+#print(days.get_days_in_month())
+
+start_day = CalendarGenerator(2026,4)
+print(start_day.get_start_day_of_month())
